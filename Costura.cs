eb047ad5a6e6527efaf85517c5d4e34e17f14252
JúliaAV <juaugustiveiga@gmail.com>
2024-07-24 09:54:24 -0300

    ;;;;;;;;;;;;;;;;;;;;;;;;;

---
+++ Costura.cs
@@ -0,0 +1,14 @@
+public class Costura
+{
+    string name;
+    Int64 id;
+
+    public void SetNome (string name)
+    {
+        this.name = name;
+    }
+    public string GetNome()
+    {
+        return name;
+    }
+}
